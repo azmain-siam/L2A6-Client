653c19c3f9f79bd7e4b068e6a103926ad900b32f
azmain-siam <siamhossain2700@gmail.com>
2025-04-15 22:38:47 +0600

    decode access token and get user

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
+import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
+import { jwtDecode } from "jwt-decode";
 
 export const registerUser = async (userData: FieldValues) => {
   try {
@@ -31,8 +33,24 @@ export const loginUser = async (loginData: FieldValues) => {
       body: JSON.stringify(loginData),
     });
 
-    return res.json();
+    const result = await res.json();
+    const storeCookies = await cookies();
+
+    if (result.success) {
+      storeCookies.set("accessToken", result.data.accessToken);
+    }
+    return result;
   } catch (error: any) {
     return Error(error);
+  }
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = (await cookies()).get("accessToken")?.value;
+  let decodedData = null;
+
+  if (accessToken) {
+    decodedData = await jwtDecode(accessToken);
+    return decodedData;
   }
 };
